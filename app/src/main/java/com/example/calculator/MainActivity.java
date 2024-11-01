7181c1dca9713c0534032968b38f2750926ad4cb
Aliaksandr Astapchyk <alastapchyk@gmail.com>
2024-11-01 14:15:03 +0100

    Expression input

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.calculator;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.os.Vibrator;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,20 +16,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    public enum Operation {
-        DIVIDE, MULTIPLY, SUBTRACT, ADD
-    }
-    Operation operationState = null;
-    int firstOperand = 0;
-    int secondOperand = 0;
-    TextView input;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-    Button clearLastButton;
+public class MainActivity extends AppCompatActivity {
+    Vibrator vibrator;
+
+    EditText input;
+    TextView result;
+    Button backspaceButton;
+    Button button1; // clearAll
+    Button button2; // Parentheses
+    Button button3; // percent
     Button button5, button6, button7, button9, button10, button11, button13, button14, button15, button18; // Numpad
-    Button button2, button3; // brackets
-    Button button4, button8, button12, button16;
-    Button buttonRes;
+    Button button4, button8, button12, button16; // operations
+    Button button17; // toggle positivity :) -> :( -> :)
+    Button button19; // point
+    Button button20; // equals
+
+    private static final Set<Character> operators = new HashSet<Character>(Arrays.asList('÷', '×', '-', '+'));
+    Integer openParenthesesCount = 0;
+
+
+    @Override
+    protected void onResume() { // Request focus on the root layout to avoid focusing the EditText by default
+        super.onResume();
+        View rootLayout = findViewById(R.id.main);
+        rootLayout.setFocusableInTouchMode(true);
+        rootLayout.requestFocus();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +58,176 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        input = findViewById(R.id.input);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
-        clearLastButton = findViewById(R.id.backspaceButton);
+        findViewByIdOfAllElements();
+
+        setButtonClickListeners();
+        setButtonTouchListeners();
+    }
+
+    private void clearAll() {
+        input.setText("0");
+        result.setText("");
+        openParenthesesCount = 0;
+        focusInput();
+    }
+
+    private void clearLast() {
+        String inputText = input.getText().toString();
+
+        if (inputText.length() > 1) {
+            Character last = inputText.charAt(inputText.length() - 1);
+            if (last.equals('(')) openParenthesesCount--;
+            else if (last.equals(')')) openParenthesesCount++;
+
+            inputText = inputText.substring(0, inputText.length() - 1);
+        } else {
+            inputText = "0";
+            openParenthesesCount = 0;
+        }
+        input.setText(inputText);
+        focusInput();
+    }
+
+    private void inputNumber(String num) {
+        String inputText = input.getText().toString();
+        Character last = inputText.charAt(inputText.length() - 1);
+        if (last.equals(')') || last.equals('%')) input.setText(inputText + "×" + num);
+        else input.setText(inputText.equals("0") ? num : inputText + num);
+        focusInput();
+    }
+
+    private void inputOperator(Character operator) {
+        String inputText = input.getText().toString();
+
+        if (inputText.equals("0") && operator.equals('-')) input.setText("-");
+        else if (inputText.equals("-")) {
+            if (operator.equals('+'))
+                input.setText("0");
+        } else if (operators.contains(inputText.charAt(inputText.length() - 1))) {
+            if (inputText.charAt(inputText.length() - 2) != '(')
+                input.setText(inputText.substring(0, inputText.length() - 1) + operator);
+        } else if (inputText.charAt(inputText.length() - 1) != '('
+                || operator == '-')
+            input.setText(inputText + operator);
+
+        focusInput();
+    }
+
+    private void inputParentheses() {
+        String inputText = input.getText().toString();
+        Character last = inputText.charAt(inputText.length() - 1);
+        boolean isLastDigit = Character.isDigit(last);
+
+        if (inputText.equals("0")) {
+            input.setText("(");
+            openParenthesesCount++;
+        } else {
+            boolean isExpression = isLastDigit || last.equals(')') || last.equals('%');
+            if (openParenthesesCount > 0) {
+                if (isExpression) {
+                    input.setText(inputText + ")");
+                    openParenthesesCount--;
+                } else {
+                    input.setText(inputText + "(");
+                    openParenthesesCount++;
+                }
+            } else {
+                if (isExpression) input.setText(inputText + "×(");
+                else input.setText(inputText + "(");
+                openParenthesesCount++;
+            }
+        }
+        focusInput();
+    }
+
+    private void inputPercent() {
+        String inputText = input.getText().toString();
+        Character last = inputText.charAt(inputText.length() - 1);
+        if (Character.isDigit(last) || last == ')')
+            input.setText(inputText + "%");
+        focusInput();
+    }
+
+    private void inputPoint() {
+        String inputText = input.getText().toString();
+        if (!isLastFloatNumber() && Character.isDigit(inputText.charAt(inputText.length() - 1)))
+            input.setText(inputText + ".");
+        focusInput();
+    }
+
+    private boolean isLastFloatNumber() {
+        String inputText = input.getText().toString();
+
+        for (int i = inputText.length() - 1; i > 0; i--) {
+            if (inputText.charAt(i) == '.') return true;
+            else if (!Character.isDigit(inputText.charAt(i))) return false;
+        }
+        return false;
+    }
+
+    private void togglePositivity() {
+        String inputText = input.getText().toString();
+
+        if (inputText.equals("0"))
+            input.setText("-");
+        else if (inputText.equals("-"))
+            input.setText("0");
+        else {
+            Character last = inputText.charAt(inputText.length() - 1);
+            boolean isExpression = Character.isDigit(last) || last.equals(')') || last.equals('%');
+
+            if (!isExpression) return;
+
+            int i = inputText.length() - 2;
+            int paranthesesCount = last.equals(')') ? 1 : 0;
+
+            while (paranthesesCount > 0 || (i >= 0 && Character.isDigit(inputText.charAt(i)))) {
+                if (inputText.charAt(i) == ')')
+                    paranthesesCount++;
+                else if (inputText.charAt(i) == '(')
+                    paranthesesCount--;
+                i--;
+            }
+
+            StringBuilder result = new StringBuilder(inputText);
+
+            if (i >= 0) {
+                if (inputText.charAt(i + 1) == '(' && inputText.charAt(i + 2) == '-') {
+                    result.delete(i + 1, i + 3);
+                    result.delete(result.length() - 1, result.length());
+                } else if (inputText.charAt(i) == '-') {
+                    if (i == 0) result.delete(0, 1);
+                    else result.setCharAt(i, '+');
+                } else if (inputText.charAt(i) == '+')
+                    result.setCharAt(i, '-');
+                else {
+                    result.insert(i + 1, "(-");
+                    result.append(")");
+                }
+
+            } else result.insert(0, '-');
+
+            input.setText(result.toString());
+        }
+
+        focusInput();
+    }
+
+    private void focusInput() {
+        if (!input.hasFocus()) input.requestFocus();
+        input.setSelection(input.getText().length());
+    }
+
+    private void findViewByIdOfAllElements() {
+        input = findViewById(R.id.input);
+        result = findViewById(R.id.result);
+
+        backspaceButton = findViewById(R.id.backspaceButton);
+
+        button1 = findViewById(R.id.button1);
+
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
         button5 = findViewById(R.id.button5);
@@ -50,21 +239,25 @@ public class MainActivity extends AppCompatActivity {
         button13 = findViewById(R.id.button13);
         button14 = findViewById(R.id.button14);
         button15 = findViewById(R.id.button15);
+
+        button17 = findViewById(R.id.button17);
         button18 = findViewById(R.id.button18);
+        button19 = findViewById(R.id.button19);
+        button20 = findViewById(R.id.button20);
 
         button4 = findViewById(R.id.button4);
         button8 = findViewById(R.id.button8);
         button12 = findViewById(R.id.button12);
         button16 = findViewById(R.id.button16);
-
-        setButtonListeners();
     }
 
-    private void setButtonListeners() {
-        clearLastButton.setOnClickListener(v -> clearLast());
+    private void setButtonClickListeners() {
+        backspaceButton.setOnClickListener(v -> clearLast());
 
-        button2.setOnClickListener(v -> inputNumber("("));
-        button3.setOnClickListener(v -> inputNumber(")"));
+        button1.setOnClickListener(v -> clearAll());
+
+        button2.setOnClickListener(v -> inputParentheses());
+        button3.setOnClickListener(v -> inputPercent());
 
         button5.setOnClickListener(v -> inputNumber("1"));
         button6.setOnClickListener(v -> inputNumber("2"));
@@ -77,35 +270,98 @@ public class MainActivity extends AppCompatActivity {
         button15.setOnClickListener(v -> inputNumber("9"));
         button18.setOnClickListener(v -> inputNumber("0"));
 
-//        button4.setOnClickListener(v -> setOperationState(Operation.DIVIDE));
-//        button8.setOnClickListener(v -> setOperationState(Operation.MULTIPLY));
-//        button12.setOnClickListener(v -> setOperationState(Operation.SUBTRACT));
-//        button16.setOnClickListener(v -> setOperationState(Operation.ADD));
+        button17.setOnClickListener(v -> togglePositivity());
+        button19.setOnClickListener(v -> inputPoint());
+
+        button4.setOnClickListener(v -> inputOperator('÷'));
+        button8.setOnClickListener(v -> inputOperator('×'));
+        button12.setOnClickListener(v -> inputOperator('-'));
+        button16.setOnClickListener(v -> inputOperator('+'));
     }
 
-    private void clearLast() {
-        if(operationState != null) {
-            operationState = null;
-            return;
+    private void setButtonTouchListeners() {
+        input.setOnTouchListener((v, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                input.requestFocus();
+                input.setSelection(input.getText().length());
+            }
+            return true;
+        });
+
+        backspaceButton.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    vibrate();
+                    clearLast();
+                    handler.postDelayed(backspaceRunnable, 300);
+                    return true;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    handler.removeCallbacks(backspaceRunnable);
+                    return true;
+            }
+            return true;
+        });
+
+        button1.setOnTouchListener(this::handleTouchEvent);
+
+        button2.setOnTouchListener(this::handleTouchEvent);
+        button3.setOnTouchListener(this::handleTouchEvent);
+
+        button5.setOnTouchListener(this::handleTouchEvent);
+        button6.setOnTouchListener(this::handleTouchEvent);
+        button7.setOnTouchListener(this::handleTouchEvent);
+        button9.setOnTouchListener(this::handleTouchEvent);
+        button10.setOnTouchListener(this::handleTouchEvent);
+        button11.setOnTouchListener(this::handleTouchEvent);
+        button13.setOnTouchListener(this::handleTouchEvent);
+        button14.setOnTouchListener(this::handleTouchEvent);
+        button15.setOnTouchListener(this::handleTouchEvent);
+        button18.setOnTouchListener(this::handleTouchEvent);
+
+        button17.setOnTouchListener(this::handleTouchEvent);
+        button19.setOnTouchListener(this::handleTouchEvent);
+        button20.setOnTouchListener(this::handleTouchEvent);
+
+        button4.setOnTouchListener(this::handleTouchEvent);
+        button8.setOnTouchListener(this::handleTouchEvent);
+        button12.setOnTouchListener(this::handleTouchEvent);
+        button16.setOnTouchListener(this::handleTouchEvent);
+    }
+
+    private final Handler handler = new Handler();
+    private final Runnable backspaceRunnable = new Runnable() {
+        @Override
+        public void run() {
+            clearLast();
+            vibrate();
+            handler.postDelayed(this, 50);
         }
-        String inputText = input.getText().toString();
-        if (inputText.length() > 1) {
-            inputText = inputText.substring(0, inputText.length() - 1);
-        } else inputText = "0";
-        input.setText(inputText);
+    };
+
+    private boolean handleTouchEvent(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                vibrate();
+                animateScale(v, 0.8f);
+                return false;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                animateScale(v, 1.0f);
+                break;
+        }
+        return false;
     }
 
-    private void inputNumber(String num) {
-        String inputText = input.getText().toString();
-        input.setText(inputText.equals("0") ? num : inputText + num);
+    private void animateScale(View view, float scale) {
+        view.animate()
+                .scaleX(scale)
+                .scaleY(scale)
+                .setDuration(50)
+                .start();
     }
 
-//    private void setOperationState(Operation o) {
-//        operationState = o;
-//        String inputText = input.getText().toString();
-//        String operationChar;
-//        input.setText(inputText + operationChar);
-//    }
-
-//    private void result()
+    private void vibrate() {
+        if (vibrator != null) vibrator.vibrate(50);
+    }
 }
